e74cfae50322616de6879a853d73b7a8efd098a7
Andrey Pavlov <andrey-mp@yandex.ru>
2025-10-22 15:06:47 +0300

    add upstream gpertools instead of patched
    Change-Id: I78cd2f1975057752f0b10206e8246c30b3a5d050

--- container/build-rocky9.sh
+++ container/build-rocky9.sh
@@ -29,8 +29,8 @@ dnf -y install \
   python3-virtualenv python3-future python3-tox python3-devel python3-lxml \
   python3-setuptools python3-distro perl-diagnostics tbb openssl openssl-devel \
   libcap-devel libnghttp2-devel boost boost-devel rapidjson-devel \
-  doxygen graphviz bison flex bzip2 patch unzip \
-  userspace-rcu-devel
+  doxygen graphviz bison flex bzip2 patch unzip userspace-rcu-devel \
+  gperftools-libs gperftools-devel rapidjson-devel hiredis-devel
 
 # build automake 1.16.5 since rocky9 provides only 1.16.2
 # required for bind-9.21.3
